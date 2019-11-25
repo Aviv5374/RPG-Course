8eb39400b969aad140bda47df0684ca80a5a411d
Aviv5374 <Aviv5374@gmail.com>
2019-11-25 14:56:33 +0200

    Introduction To Raycasting

--- RPG Project/Assets/_My Assets/Scripts/Mover.cs
+++ RPG Project/Assets/_My Assets/Scripts/Mover.cs
@@ -7,17 +7,25 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    Ray lastRay;
+    Camera mainCamera;
     NavMeshAgent myMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
         myMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        myMeshAgent.destination = target.position;
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red);
+        myMeshAgent.destination = target.position;      
     }
 }
