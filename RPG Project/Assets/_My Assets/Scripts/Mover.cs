09b057aa89233fbc30a1a1996f0c0bebc472c6fd
Aviv5374 <Aviv5374@gmail.com>
2019-11-27 10:07:00 +0200

    Match Animation To Movement

--- RPG Project/Assets/_My Assets/Scripts/Mover.cs
+++ RPG Project/Assets/_My Assets/Scripts/Mover.cs
@@ -9,12 +9,15 @@ public class Mover : MonoBehaviour
    
     Camera mainCamera;
     NavMeshAgent myMeshAgent;
+    Animator myAnimator;
+    List<AnimatorControllerParameter> myAnimtorParameters = new List<AnimatorControllerParameter>();
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
         myMeshAgent = GetComponent<NavMeshAgent>();
+        SetMyAnimator();
     }
 
     // Update is called once per frame
@@ -23,7 +26,18 @@ public class Mover : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             MoveToCurser();
-        }       
+        }
+
+        UpdateAnimator();
+    }
+
+    void SetMyAnimator()//TODO:FINE Better Method Name
+    {
+        myAnimator = GetComponent<Animator>();
+        for (int index = 0; index < myAnimator.parameterCount; index++)
+        {
+            myAnimtorParameters.Add(myAnimator.GetParameter(index));
+        }
     }
 
     void MoveToCurser()
@@ -33,7 +47,20 @@ public class Mover : MonoBehaviour
         bool hasHit = Physics.Raycast(lastRay,out hitInfo);
         if (hasHit)
         {
-            myMeshAgent.destination = hitInfo.point;
+            myMeshAgent.destination = hitInfo.point;            
         }
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 velocity = myMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        UpdateLocomotion(speed);
+    }
+
+    void UpdateLocomotion(float speed)
+    {
+        myAnimator.SetFloat(myAnimtorParameters[0].name, speed);
     }
 }
