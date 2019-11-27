9b55e8bd6bf46a4ec12d32716b1746b130d8118d
Aviv5374 <Aviv5374@gmail.com>
2019-11-27 15:36:44 +0200

    Raycasting For Components

--- RPG Project/Assets/_My Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/_My Assets/Scripts/Control/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control
 {
@@ -9,14 +10,40 @@ namespace RPG.Control
     {
         Camera mainCamera;
         Mover mover;
+        Fighter fighter;
+
+        Ray MouseRay { get { return mainCamera.ScreenPointToRay(Input.mousePosition); } }
 
         void Start()
         {
             mainCamera = Camera.main;
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         void Update()
+        {
+            //DOTO: try to Raycast once for all uses
+            InteractWithCombat();
+            InteractWithMovement();            
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(MouseRay);
+            foreach (RaycastHit hitInfo in hits)
+            {
+                //Debug.Log(hitInfo.transform.gameObject.name);
+                CombatTarget target = hitInfo.transform.GetComponent<CombatTarget>();
+
+                if (target && Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            } 
+        }
+
+        void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -26,9 +53,8 @@ namespace RPG.Control
 
         void MoveToCursor()
         {
-            RaycastHit hitInfo;
-            Ray lastRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-            bool hasHit = Physics.Raycast(lastRay, out hitInfo);
+            RaycastHit hitInfo;           
+            bool hasHit = Physics.Raycast(MouseRay, out hitInfo);
             if (hasHit)
             {
                 mover.MoveTo(hitInfo.point);
