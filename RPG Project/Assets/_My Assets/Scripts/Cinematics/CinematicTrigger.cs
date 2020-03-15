2f3c9d958813addcd9804e8f895cd5ef857a0bd0
Aviv5374 <aviv5374@gmail.com>
2020-03-15 14:34:38 +0200

    Trigger Cinemachine Cut Scene

---
+++ RPG Project/Assets/_My Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    [RequireComponent(typeof(PlayableDirector))]
+    public class CinematicTrigger : MonoBehaviour
+    {
+        [SerializeField] int maxTimeCinematicCanTrigger = 1;
+        int sumTimeCinematicTrigger = 0;
+        //OR
+        bool alreadyTriggered = false;
+
+        PlayableDirector myPlayableDirector;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            myPlayableDirector = GetComponent<PlayableDirector>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player && sumTimeCinematicTrigger < maxTimeCinematicCanTrigger && !alreadyTriggered)
+            {
+                sumTimeCinematicTrigger++;
+                //OR
+                alreadyTriggered = true;
+                //Always
+                myPlayableDirector.Play();
+            }
+        }
+    }
+}
