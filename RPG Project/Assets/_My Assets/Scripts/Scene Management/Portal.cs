8c1e404140265637775df39838514279bf995a92
Aviv5374 <aviv5374@gmail.com>
2020-03-19 09:57:57 +0200

    Scene Loading Portals

---
+++ RPG Project/Assets/_My Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using RPG.Control;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
