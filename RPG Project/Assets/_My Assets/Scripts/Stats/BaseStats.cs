729cb425015cfe1ebccb2556584f9a8cc17b6de6
Aviv5374 <aviv5374@gmail.com>
2020-04-01 17:37:28 +0300

    Progression Scriptable Object

--- RPG Project/Assets/_My Assets/Scripts/Stats/BaseStats.cs
+++ RPG Project/Assets/_My Assets/Scripts/Stats/BaseStats.cs
@@ -9,6 +9,7 @@ namespace RPG.Stats
         [Range(1, 99)]
         [SerializeField] int startingLevel = 1;
         [SerializeField] CharacterClass characterClass;
+        [SerializeField] Progression progression = null;
 
         #region Initialization
 
