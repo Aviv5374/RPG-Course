ca48de88f03e4493c5b8ea8cfd4fa5571a185f1c
Aviv5374 <aviv5374@gmail.com>
2020-04-01 17:01:36 +0300

    Pickup Respawning

--- RPG Project/Assets/_My Assets/Scripts/Combat/WeaponPickup.cs
+++ RPG Project/Assets/_My Assets/Scripts/Combat/WeaponPickup.cs
@@ -8,6 +8,19 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5;
+
+        Collider myCollider;
+        List<Transform> myChildrens = new List<Transform>();
+
+        void Awake()
+        {
+            myCollider = GetComponent<Collider>();
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                myChildrens.Add(transform.GetChild(i));
+            }
+        }
 
         //I prefer EquipWeapon() be called through the PlayerController rather than directly through Fighter
         private void OnTriggerEnter(Collider other)
@@ -17,7 +30,23 @@ namespace RPG.Combat
             {
                 Fighter fighter = player.GetComponent<Fighter>();
                 fighter.EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        void ShowPickup(bool shouldShow)
+        {
+            myCollider.enabled = shouldShow;
+            for (int i = 0; i < myChildrens.Count; i++)
+            {
+                myChildrens[i].gameObject.SetActive(shouldShow);
             }
         }
     }
