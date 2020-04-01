34dbdef519b0af8c76d2277714248a6f84d4e4d0
Aviv5374 <aviv5374@gmail.com>
2020-04-01 17:18:23 +0300

    Progression Design

---
+++ RPG Project/Assets/_My Assets/Scripts/Stats/CharacterClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+	public enum CharacterClass
+	{
+		Character,
+		Player,
+		Gurnd,
+		Grunt,
+		Mage,
+		Archer
+	}
+}
