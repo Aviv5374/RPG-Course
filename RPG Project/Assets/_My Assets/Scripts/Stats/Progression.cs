15891491bf49456b3c4970ff58d68a519960592c
Aviv5374 <aviv5374@gmail.com>
2020-04-05 16:47:33 +0300

    Performant Lookups With Dictionaries

--- RPG Project/Assets/_My Assets/Scripts/Stats/Progression.cs
+++ RPG Project/Assets/_My Assets/Scripts/Stats/Progression.cs
@@ -7,15 +7,13 @@ namespace RPG.Stats
 	[CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
 	public class Progression : ScriptableObject
 	{
-        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
-
         [System.Serializable]
         class ProgressionCharacterClass
         {
             [SerializeField] CharacterClass characterClass;
-            [SerializeField] ProgressionStat[] stats;             
+            [SerializeField] ProgressionStat[] stats;
 
-            public CharacterClass CharacterClass { get => characterClass; }            
+            public CharacterClass CharacterClass { get => characterClass; }
             public ProgressionStat[] Stats { get => stats; }
         }
 
@@ -25,35 +23,48 @@ namespace RPG.Stats
             [SerializeField] Stat stat;
             [SerializeField] float[] levels;
 
-            public Stat Stat { get => stat;}
+            public Stat Stat { get => stat; }
             public float[] Levels { get => levels; }
         }
 
+        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
+
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+        
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
+            BuildLookup();
+
+            float[] levels = lookupTable[characterClass][stat];
+            bool isLevelInRange = level > -1 && level <= levels.Length;
+
+            if (isLevelInRange)
+            {
+                return levels[level - 1];
+            }
+            else
+            {
+                return 0;
+            }            
+        }
+
+        void BuildLookup()
+        {
+            if (lookupTable != null) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
             for (int index1 = 0; index1 < characterClasses.Length; index1++)
             {
-                if (characterClasses[index1].CharacterClass != characterClass) { continue; }
-
+                var statLookupTable = new Dictionary<Stat, float[]>();
                 ProgressionStat[] stats = characterClasses[index1].Stats;
-
                 for (int index2 = 0; index2 < stats.Length; index2++)
                 {
-                    #region option 1
-                    //if(stats[index2].Stat != stat) { continue; }
-                    //if (level <= -1) { continue; }
-                    //if (level > stats[index2].Levels.Length) { continue; }
-                    #endregion
-
-                    bool isLevelInRange = level > -1 && level <= stats[index2].Levels.Length;
-                    if (stats[index2].Stat == stat && isLevelInRange)
-                    {
-                        return stats[index2].Levels[level - 1];
-                    }
+                    statLookupTable[stats[index2].Stat] = stats[index2].Levels;
                 }
 
+                lookupTable[characterClasses[index1].CharacterClass] = statLookupTable;
             }
-            return 0;
         }
 
     }
