bcb60a41823a3b2c5306fe82a21bdcdfc7498552
Aviv5374 <aviv5374@gmail.com>
2020-04-13 14:51:00 +0300

    In Game UI

---
+++ RPG Project/Assets/_My Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+
+        Camera mainCamera = null;
+
+        #region Initialization
+
+        void Awake()
+        {
+            mainCamera = Camera.main;
+        }
+
+        void Start()
+        {
+
+        }
+
+        #endregion
+
+        #region Updating	 
+
+        void Update()
+        {
+            transform.forward = mainCamera.transform.forward;
+        }
+
+        #endregion
+
+    }
+}
