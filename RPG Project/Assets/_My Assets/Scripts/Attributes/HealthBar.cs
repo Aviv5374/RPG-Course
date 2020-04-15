3d792355e143c008158dc2fc5973986918027dd0
Aviv5374 <aviv5374@gmail.com>
2020-04-15 10:51:23 +0300

    Disabling Empty Health Bars

--- RPG Project/Assets/_My Assets/Scripts/Attributes/HealthBar.cs
+++ RPG Project/Assets/_My Assets/Scripts/Attributes/HealthBar.cs
@@ -8,12 +8,22 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
-        
+        [SerializeField] Canvas rootCanvas = null;
+
+
+        bool IsHealtBarFull { get { return Mathf.Approximately(healthComponent.HealFraction, 1); } }
+        bool IsHealtBarEmpty { get { return Mathf.Approximately(healthComponent.HealFraction, 0); } }
 
         #region Updating	 
 
         void Update()
         {
+            if (IsHealtBarFull || IsHealtBarEmpty)
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
             foreground.localScale = new Vector3(healthComponent.HealFraction, 1, 1);
         }
 
