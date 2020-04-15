0a4ac6c3c20f1693224919feac3e33bd205b0b90
Aviv5374 <aviv5374@gmail.com>
2020-04-15 15:37:09 +0300

    Boom Microphone Audio Listener

--- RPG Project/Assets/_My Assets/Scripts/Combat/WeaponPrefab.cs
+++ RPG Project/Assets/_My Assets/Scripts/Combat/WeaponPrefab.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class WeaponPrefab : MonoBehaviour
     {
-       public void OnHit()
-       {
+        [SerializeField] UnityEvent onHit;
+
+        public void OnHit()
+        { 
             Debug.Log("Weapon Hit " + name);
+            onHit.Invoke();
         }
     }
 }
